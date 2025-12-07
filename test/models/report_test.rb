54bc57abb64b3f728a40c97c6e3fb24046ea62e7
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2025-12-07 19:26:57 +0900

    Reportモデルのテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,25 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    target_user = users(:alice)
+    report = reports(:alice_report)
+    assert report.editable?(target_user)
+  end
+
+  test '#created_on' do
+    report = reports(:alice_report)
+    assert_equal Date.new(2025, 12, 6), report.created_on
+  end
+
+  test '#save_mentions' do
+    alice_report = reports(:alice_report)
+    reports(:bob_report)
+    charlie_report = reports(:charlie_report)
+    report_mentions(:alice_to_bob)
+    report_mentions(:alice_to_charlie)
+    assert_difference 'alice_report.mentioning_reports.count', -1 do
+      alice_report.update!(content: "http://localhost:3000/reports/#{charlie_report.id}")
+    end
+  end
 end
